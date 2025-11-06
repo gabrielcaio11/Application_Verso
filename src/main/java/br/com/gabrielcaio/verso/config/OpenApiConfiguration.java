b5b12a128b6e03a0b78aa86d9418c92990ff2252
gabrielcaio11 <gabrielcaio848@gmail.com>
2025-11-06 15:16:10 -0300

    feat(openapi): enhance OpenAPI configuration with basic authentication security scheme

--- src/main/java/br/com/gabrielcaio/verso/config/OpenApiConfiguration.java
+++ src/main/java/br/com/gabrielcaio/verso/config/OpenApiConfiguration.java
@@ -1,9 +1,12 @@
 package br.com.gabrielcaio.verso.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,24 +15,31 @@ import java.util.List;
 
 @Configuration
 public class OpenApiConfiguration {
+    private static final String SECURITY_SCHEME_NAME = "basicAuth";
 
-        @Bean
-        public OpenAPI customOpenAPI() {
-                return new OpenAPI()
-                                .info(new Info()
-                                                .title("Application Verso")
-                                                .version("v1")
-                                                .description("API REST para sistema de criação, publicação e gerenciamento de artigos")
-                                                .contact(new Contact()
-                                                                .name("Gabriel Caio")
-                                                                .email("gabrielcaio848@gmail.com")
-                                                                .url("https://verso.com"))
-                                                .license(new License()
-                                                                .name("Apache 2.0")
-                                                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
-                                .servers(List.of(
-                                                new Server()
-                                                                .url("http://localhost:8080")
-                                                                .description("Ambiente Local")));
-        }
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components()
+                        .addSecuritySchemes(SECURITY_SCHEME_NAME,
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")))
+                .addSecurityItem(new SecurityRequirement().addList(SECURITY_SCHEME_NAME))
+                .info(new Info()
+                        .title("Application Verso")
+                        .version("v1")
+                        .description("API REST para sistema de criação, publicação e gerenciamento de artigos")
+                        .contact(new Contact()
+                                .name("Gabriel Caio")
+                                .email("gabrielcaio848@gmail.com")
+                                .url("https://verso.com"))
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("https://www.apache.org/licenses/LICENSE-2.0.html")))
+                .servers(List.of(
+                        new Server()
+                                .url("http://localhost:8080")
+                                .description("Ambiente Local")));
+    }
 }
