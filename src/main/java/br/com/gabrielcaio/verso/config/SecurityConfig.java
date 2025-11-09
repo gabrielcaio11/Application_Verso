6eff15161bfed9517e99c4da6fd9c002bf42b610
gabrielcaio11 <gabrielcaio848@gmail.com>
2025-11-09 17:32:33 -0300

    fix: adjust PMD configuration to remove deprecated ruleset (Closes #11)

--- src/main/java/br/com/gabrielcaio/verso/config/SecurityConfig.java
+++ src/main/java/br/com/gabrielcaio/verso/config/SecurityConfig.java
@@ -8,7 +8,6 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.core.GrantedAuthorityDefaults;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -43,17 +42,6 @@ public class SecurityConfig {
                 .httpBasic(Customizer.withDefaults())
                 .build();
     }
-
-
-//    @Bean
-//    public WebSecurityCustomizer webSecurityCustomizer() {
-//        return (web) -> web.ignoring()
-//                .requestMatchers("/swagger-ui/**")
-//                .requestMatchers("/swagger-ui.html")
-//                .requestMatchers("/v3/api-docs/**")
-//                .requestMatchers("/api-docs/**")
-//                .requestMatchers("/webjars/**");
-//    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
